887c6c4f0b3cb25fe2d3ae87702d669dd59da2f6
kbwin1 <kbwinjob@yahoo.com>
2024-03-31 01:59:06 -0400

    first commit

---
+++ venv/models.py
@@ -0,0 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+def connect_db(app):
+  db.app = app
+  db.init_app(app)
+
+
+  
+class User(db.Model):
+  __tablename__ ='users'
+  
+  user_id=db.Column(db.Integer,
+               primary_key=True,
+               autoincrement=True)
+  
+  First_Name = db.Column(db.String(20),
+                     nullable=False,)
+  
+  Last_Name = db.Column(db.String(20),
+                     nullable=False,)
+  
+  Profile_Pic = db.Column(db.String(2000),
+                     nullable=False,)
+  
+  owner = db.relationship('Post')
+  
+class Post(db.Model):
+  
+  __tablename__='post'
+  
+  id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+  
+  title=db.Column(db.String(100),
+                     nullable=False,)
+  
+  comment=db.Column(db.String(1000),
+                     nullable=False,)
+  
+  user_ref =db.Column(db.Integer, db.ForeignKey('users.user_id'))
+  
+  owner_id = db.relationship('User')
---
+++ venv/seeds.py
@@ -0,0 +1,28 @@
+from models import User, db, Post
+from app import app
+
+## CREATE ALL TABLES
+db.drop_all()
+db.create_all()
+
+#IF table isnt empty empty it all
+User.query.delete()
+
+#add data 
+William = User(First_Name='William',Last_Name='Gonzales',Profile_Pic='https://publish-p47754-e237306.adobeaemcloud.com/adobe/dynamicmedia/deliver/dm-aid--e7154f9f-8a49-4afb-a8f1-278d54ae6a76/_330587629904.app.png?preferwebp=true&width=420')
+Juan = User(First_Name='Bowser', Last_Name='Jimenes', Profile_Pic='https://publish-p47754-e237306.adobeaemcloud.com/adobe/dynamicmedia/deliver/dm-aid--323b5cf8-c89f-4158-addd-80b45571ff09/_383195890566.app.png?preferwebp=true&width=420')
+Pedro = User(First_Name='Pedro', Last_Name='Gomez', Profile_Pic='https://publish-p47754-e237306.adobeaemcloud.com/adobe/dynamicmedia/deliver/dm-aid--503de83a-62df-4c8a-b047-bedb22002ae4/_405531873648.app.png?preferwebp=true&width=420')
+
+post1= Post(title="First Post", comment='First comment', user_ref = 1)
+#add new object to session so they will saty
+
+db.session.add(William)
+db.session.add(Juan)
+db.session.add(Pedro)
+
+db.session.commit()
+
+db.session.add(post1)
+
+db.session.commit()
+
